bfcb9b2635c6898376e163f0b37a35e04261337d
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-01-11 17:31:49 +0300

    start of simplifying

---
+++ includes/footer.php
@@ -0,0 +1,155 @@
+<footer>
+        <!--? Footer Start-->
+        <div class="footer-area footer-bg">
+            <div class="container">
+                <div class="footer-top footer-padding">
+                    <!-- Footer Menu -->
+                    <div class="row d-flex justify-content-between">
+                        <div class="col-sm-3">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4>Company</h4>
+                                    <ul>
+                                        <li><a href="aboutus.php" style="color:white">About Us</a></li>
+                                        <li><a href="missionandvision.php" style="color:white">Mission & Vision</a>
+                                        </li>
+                                        <li><a href="corevalues.php" style="color:white">Core Values</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4>Services</h4>
+                                    <ul>
+                                        <li><a href="sustainableagriculturalconsultancy.php"
+                                                style="color:white">SUSTAINABLE AGRICULTURAL CONSULTANCY</a></li>
+                                        <li><a href="foodwasterecyclingindustry.php" style="color:white">FOOD WASTE
+                                                RECYCLING INDUSTRY</a></li>
+                                        <li><a href="foodbankingfoodsecurity.php" style="color:white">FOOD BANKING &
+                                                FOOD SECURITY CONSULTANCY</a></li>
+                                        <li><a href="eosphconsultancy.php" style="color:white">ENVIRONMENTAL,
+                                                OCCUPATIONAL SAFETY & PUBLIC HEALTH CONSULTANCY</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4>Quick Links</h4>
+                                    <ul>
+                                        <li><a href="contactus.php" style="color:white">Contact Us</a></li>
+                                        <li><a href="team.php" style="color:white">Our Team</a></li>
+                                        <li><a href="ourgoals.php" style="color:white">Our Goals</a></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="single-footer-caption mb-50">
+                                <div class="footer-tittle">
+                                    <h4>Let’s Get Social</h4>
+                                    <!-- <a href="http://www.pomilly.com/webmail" class="genric-btn primary e-large">Staff
+                                            Email
+                                        </a> -->
+                                    <div class="footer-social mt-3">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                        <a href="#"><i class="fab fa-youtube"></i></a>
+                                        <a style="background: rgb(136, 136, 50);" href="#"><i class="fab fa-linkedin"></i></a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- footer Heading -->
+
+
+                </div>
+                <!-- Footer Bottom -->
+                <div class="footer-bottom">
+                    <div class="row d-flex align-items-center">
+                        <div class="col-lg-12">
+                            <div class="footer-copy-right text-center">
+                                <p>
+                                    <!-- Copyright &copy;<script>
+                                        document.write(new Date().getFullYear());
+                                    </script> All rights reserved -->
+                                    Copyright © Pomilly East African Limited
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- Footer End-->
+    </footer>
+    <!-- Scroll Up -->
+    <div id="back-top">
+        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+    </div>
+     <!-- GetButton.io widget -->
+     <script type="text/javascript">
+        (function () {
+        var options = {
+        whatsapp: "+254 745 462781", // WhatsApp number
+        call_to_action: "Message us", // Call to action
+        position: "left", // Position may be 'right' or 'left'
+        pre_filled_message: "Hi there 👋 Welcome to Pomilly East Africa Limited", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+        </script>
+<!-- /GetButton.io widget -->
+    <!-- JS here -->
+
+    <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- Jquery, Popper, Bootstrap -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Jquery Mobile Menu -->
+    <script src="assets/js/jquery.slicknav.min.js"></script>
+
+    <!-- Jquery Slick , Owl-Carousel Plugins -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <!-- One Page, Animated-HeadLin -->
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/animated.headline.js"></script>
+    <script src="assets/js/jquery.magnific-popup.js"></script>
+
+    <!-- Date Picker -->
+    <script src="assets/js/gijgo.min.js"></script>
+    <!-- Nice-select, sticky -->
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.sticky.js"></script>
+
+    <!-- counter , waypoint -->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+
+    <!-- contact js -->
+    <script src="assets/js/contact.js"></script>
+    <script src="assets/js/jquery.form.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/mail-script.js"></script>
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <!-- Jquery Plugins, main Jquery -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
